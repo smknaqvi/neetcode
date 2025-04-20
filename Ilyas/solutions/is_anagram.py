b0da2626df5b2925a0586f3269581f876978dfcc
Jafer Haider <itsjafer@gmail.com>
2025-04-19 23:45:08 -0500

    Update is_anagram.py

--- Ilyas/solutions/is_anagram.py
+++ Ilyas/solutions/is_anagram.py
@@ -1,5 +1,15 @@
 from collections import defaultdict
 
 def isAnagram(self, s: str, t: str) -> bool:
-    # TODO: Implement your solution here
-    pass
+    if len(s) != len(t):
+        return False
+      
+    dictS = dict()
+    dictT = dict()
+    for i in s:
+        dictS[i] = dictS.get(i, 0) + 1
+  
+    for j in t:
+        dictT[j] = dictT.get(j, 0) + 1
+  
+    return dictT == dictS
